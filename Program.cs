ed6c026da71441d7482c28349949d86f55c43de6
Ak47u1999 <arpittopper.2017@gmail.com>
2021-10-16 23:34:18 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RandomReviewGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<=100000;i++)
+            {
+                Random r = new Random();
+                int rInt = r.Next(1, 10);
+                Console.WriteLine($"{rInt}");
+            }
+        }
+    }
+}
